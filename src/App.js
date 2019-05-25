75408982ddd29bc509178af4642f65d5a7ac68a5
axl <whodakillabee@gmail.com>
2019-05-25 13:46:52 +0200

    adds amplify

--- src/App.js
+++ src/App.js
@@ -1,26 +1,32 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect, useState } from "react";
+import "./App.css";
+
+import { API, graphqlOperation } from "aws-amplify";
+import { withAuthenticator } from "aws-amplify-react";
+
+import { listPosts } from "./graphql/queries";
 
 function App() {
+  const [posts, updatePosts] = useState([]);
+  useEffect(() => {
+    getData();
+  });
+  async function getData() {
+    try {
+      const postsData = await API.graphql(graphqlOperation(listPosts));
+      updatePosts(postsData.data.listPosts.items);
+    } catch (error) {
+      console.log("error fetching data...", error);
+    }
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {posts.map(p => (
+        <h2>{p.title}</h2>
+      ))}
     </div>
   );
 }
 
-export default App;
+export default withAuthenticator(App, { includeGreetings: true });
